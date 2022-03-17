5d6385fe39114480833c73ad05c88c0942564376
ygoto <y.goto@cybermissions.co.jp>
2022-03-17 16:36:34 +0900

    問題文の全文を表示可能に

--- components/question-invidual.vue
+++ components/question-invidual.vue
@@ -1,9 +1,34 @@
 <template>
   <v-container>
     <v-card>
-      <v-text-title>
+      <v-card-title v-if="question.class === 'A'">
+        A.あなたの仕事についてうかがいます
+      </v-card-title>
+      <v-card-title v-else-if="question.class === 'B'">
+        B.最近1か月間のあなたの状態についてうかがいます
+      </v-card-title>
+      <v-card-title v-else-if="question.class === 'C'">
+        C.あなたの周りの方々についてうかがいます
+      </v-card-title>
+      <v-card-title v-else>
+        D.満足度について
+      </v-card-title>
+      <br>
+      <v-card-text v-if="question.question === 'C1'">
+        次の人たちはどのくらい気軽に話ができますか?
+      </v-card-text>
+      <v-card-text v-else-if="question.question === 'C2'">
+        あなたが困った時、次の人たちはどのくらい頼りになりますか?
+      </v-card-text>
+      <v-card-text v-else-if="question.question === 'C3'">
+        あなたの個人的な問題を相談したら、次の人たちはどのくらいきいてくれますか?
+      </v-card-text>
+      <v-card-text v-else>
+      </v-card-text>
+      <br>
+      <v-card-text>
         {{ question.content }}
-      </v-text-title>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -18,6 +43,7 @@ export default {
   },
   data () {
     return {
+      class: ''
     }
   }
 }
--- components/question-list.vue
+++ components/question-list.vue
@@ -35,41 +35,6 @@ export default {
     return {
       onboarding: 0,
       questions: [],
-      context: [
-        {
-          class: 'A',
-          text: 'あなたの仕事についてうかがいます'
-        },
-        {
-          class: 'B',
-          text: '最近1か月間のあなたの状態についてうかがいます'
-        },
-        {
-          class: 'C',
-          text: 'あなたの周りの方々についてうかがいます'
-        },
-        {
-          class: 'D',
-          text: '満足度について'
-        }
-      ],
-      questionText: [
-        {
-          class: 'C',
-          question: 'C1',
-          text: '次の人たちはどのくらい気軽に話ができますか?'
-        },
-        {
-          class: 'C',
-          question: 'C2',
-          text: 'あなたが困った時、次の人たちはどのくらい頼りになりますか?'
-        },
-        {
-          class: 'C',
-          question: 'C3',
-          text: 'あなたの個人的な問題を相談したら、次の人たちはどのくらいきいてくれますか?'
-        }
-      ]
     }
   },
   computed: {
